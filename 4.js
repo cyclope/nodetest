c8f42cba4dedf5ddb9744459977537ff2019e63d
cyclope <cyclope-laptop.pyramid>
2012-07-10 19:42:18 +0300

    todo added

--- 4.js
+++ 4.js
@@ -5,7 +5,9 @@ browser.get('/index.php?action=Login&module=Users&login_module=Home&login_action
   var form = $('form#form');
   $('#user_name', form).val( 'admin');
   $('#user_password', form).val( 'qwerty');
+    /** @todo: incorrect request reported by apache  */
   $('form#form').submit(function(){
+  
     browser.get( '/index.php?module=Home&action=index', function( res,$ ){
 	var ev = $( 'body' );
 	console.log( $(ev).html() );
