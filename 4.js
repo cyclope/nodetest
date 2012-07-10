5c28a4fb9df6d9d1f69686894f9d0df62c7f95b7
cyclope <cyclope@yandex.ru>
2012-07-10 19:58:11 +0300

    todo

--- 4.js
+++ 4.js
@@ -5,6 +5,7 @@ browser.get('/index.php?action=Login&module=Users&login_module=Home&login_action
   var form = $('form#form');
   $('#user_name', form).val( 'admin');
   $('#user_password', form).val( 'qwerty');
+  /** @todo fix submit  */
   $('form#form').submit(function(){
     browser.get( '/index.php?module=Home&action=index', function( res,$ ){
 	var ev = $( 'body' );
